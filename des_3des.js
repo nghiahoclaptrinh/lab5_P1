90a7c475e299435eff1a26c8a601bc86e2a34682
Nhaan2003 <hoainhan2904@gmail.com>
2026-04-20 13:45:08 +0700

    tạo file des_3des.js với cấu trúc hàm ban đầu

---
+++ des_3des.js
@@ -0,0 +1,26 @@
+/**
+ * Module: Mã hóa DES và 3DES
+ * Thành viên: Nhân
+ * Thư viện: CryptoJS
+ */
+
+/**
+ * Tạo khóa ngẫu nhiên cho DES/3DES
+ * @returns {string} Chuỗi hex
+ */
+function generateDESKey() {
+    // TODO: implement
+}
+
+/**
+ * Mã hóa / Giải mã bằng DES hoặc 3DES
+ * @param {string} algo   - "des" | "3des"
+ * @param {string} mode   - "cbc" | "ecb"
+ * @param {string} key    - Khóa bí mật
+ * @param {string} action - "encrypt" | "decrypt"
+ * @param {string} input  - Dữ liệu đầu vào
+ * @returns {string} Kết quả
+ */
+function runDES(algo, mode, key, action, input) {
+    // TODO: implement
+}
